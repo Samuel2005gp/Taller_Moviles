35d1e6c5f2f35b90e0d3485750f632c8eb8e648f
Samuel2005gp <sg2503818@gmail.com>
2025-10-17 14:01:53 -0500

    Add files via upload

---
+++ Taller/Punto5.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  double? a;
+  double? b;
+
+  print('Ingrese el valor de a: ');
+  a = double.parse(stdin.readLineSync()!);
+
+  print('Ingrese el valor de b: ');
+  b = double.parse(stdin.readLineSync()!);
+
+  double h = sqrt(pow(a, 2) + pow(b, 2));
+  print('\nEl valor de la hipotenusa es: ${h.toStringAsFixed(2)}');
+}
---
+++ Taller/Punto6.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+double celsius = 70;
+
+double fahrenheit = (celsius * 1.8) + 32;
+print('$celsius grados Celsius son $fahrenheit grados Fahrenheit');
+
+}
